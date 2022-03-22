5e73d9336123e4be9f930c8f3e671be101c7feef
Jack-WebDev <Jackbuda31@gmail.com>
2022-03-22 23:57:01 +0200

    Initial commit

---
+++ sunny.js
@@ -0,0 +1,8 @@
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav__menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
